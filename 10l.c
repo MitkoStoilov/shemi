5cf847ee028fea84848d84020f09e2f72e20be9e
MitkoStoilov <dstoilovk@gmail.com>
2018-11-13 22:39:43 +0200

    Create 10l.c

---
+++ 10l.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#define BUFFER_SIZE 100
+int main(){
+	char buff;
+	int count = 0;
+	int w_result;
+	int a=0;
+	
+	int fd = open("files.c", O_APPEND);
+	int result = 1;
+	
+	while(result > 0){
+		if(buff=='\n'){
+			count++;
+			if(count==10){
+				break;
+			}
+
+		}
+		result = read(fd, &buff, 1);
+		int written = 0;
+
+		while(written < result){
+			w_result = write(STDOUT_FILENO, &buff, result - written);
+
+			written += w_result;
+			
+		}
+		
+	}
+}
